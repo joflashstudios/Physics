cc8031156f1e2fb3e8f9dbc430f17e04abf117e3
joflashstudios <joflashstudios@gmail.com>
2014-11-24 22:06:02 -0500

    Added versions

---
+++ Physics/Physics/Core/AdditionMachine.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+using Numerics;
+
+namespace Physics.Core
+{
+    class AdditionMachine : Machine
+    {
+        public AdditionMachine()
+        {
+            Inputs.Add("A", new InputNode());
+            Inputs.Add("B", new InputNode());
+            Outputs.Add("C", new OutputNode());
+
+            A = Inputs["A"];
+            B = Inputs["B"];
+            C = Outputs["C"];
+        }
+
+        public InputNode A;
+        public InputNode B;
+        public OutputNode C;
+
+        public override void Compute()
+        {
+            BigRational c = A.Value.Number + B.Value.Number;
+            C.Value = new Value(c, null);
+        }
+    }
+}
---
+++ Physics/PhysicsService/Version.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Xml.Serialization;
+using System.IO;
+
+namespace PhysicsService
+{
+    public class Version
+    {
+        public string Version { get; set; }
+        public double VersionNumber
+        {
+            get
+            {
+                return double.Parse("0." + Version.Replace(".", ""));
+            }
+        }
+        public string Title { get; set; }
+        public string Notes { get; set; }
+        public string URL { get; set; }
+
+        public static Version[] GetVersions()
+        {
+            string xml = File.ReadAllText("versions.xml");
+            return DeserializeVersions(xml);
+        }
+
+        public static Version GetLatest()
+        {            
+            return GetVersions().OrderBy(n => n.VersionNumber).First();
+        }
+
+        public static void SaveVersions(Version[] versions)
+        {
+            string xml = SerializeVersions(versions);
+            File.WriteAllText("versions.xml", xml);
+        }
+
+        private static Version[] DeserializeVersions(string xml)
+        {
+            using (StringReader stream = new StringReader(xml))
+            {
+                return (Version[])Serializer.Deserialize(stream);
+            }
+        }
+
+        private static string SerializeVersions(Version[] versions)
+        {
+            using (StringWriter stream = new StringWriter())
+            {
+                Serializer.Serialize(stream, versions);
+                return stream.ToString();
+            }
+        }
+
+        private static XmlSerializer Serializer = new XmlSerializer(typeof(Version[]));
+    }
+}
